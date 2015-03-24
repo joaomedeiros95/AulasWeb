28c72370198aad433c0ed1aa4e40447e534f8967
Aluno <aluno@imd.(none)>
2015-03-24 15:19:31 -0300

    Adicionado posts em PHP

---
+++ posts/tecnologias.php
@@ -0,0 +1,11 @@
+<?php 
+	include_once('../header.php');
+	$title = 'Métodos';
+?>
+
+
+	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</p>
+	<center><img src="../imgs/2.jpg" width="50%" height="50%" /></center>
+
+
+<?php include_once('../footer.php') ?>
